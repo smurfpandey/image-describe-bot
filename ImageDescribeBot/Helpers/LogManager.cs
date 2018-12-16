65c2b663c19012a6fe203de282ef30d89cb7f536
Neeraj Verma <neeraj.viman@gmail.com>
2018-12-16 21:33:18 +0530

    adding logmanager

---
+++ ImageDescribeBot/Helpers/LogManager.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ImageDescribeBot
+{
+    public interface ILogger
+    {
+        void Debug(object message);
+        bool IsDebugEnabled { get; }
+
+        // continue for all methods like Error, Fatal ...
+    }
+
+    public class Log4NetWrapper : ILogger
+    {
+        private readonly log4net.ILog _logger;
+
+        public Log4NetWrapper(Type type)
+        {
+            _logger = log4net.LogManager.GetLogger(type);
+        }
+
+        public void Debug(object message)
+        {
+            _logger.Debug(message);
+        }
+
+        public bool IsDebugEnabled
+        {
+            get { return _logger.IsDebugEnabled; }
+        }
+
+        // complete ILogger interface implementation
+    }
+
+    public static class LogManager
+    {
+        public static ILogger GetLogger(Type type) =>            
+            new Log4NetWrapper(type);
+    }
+}
