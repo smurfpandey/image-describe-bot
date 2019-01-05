41b1779f286383a0164c8d891b525ed4f254decf
Neeraj Verma <neeraj.viman@gmail.com>
2019-01-06 00:27:10 +0530

    everything sucks

---
+++ ReportAPI/Helpers/MongoHelper.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Extensions.Configuration;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ReportAPI
+{
+    public class MongoHelper
+    {
+        private static readonly string CONNECTION_STRING_NAME = "ImageDescBotDb";
+        private static readonly string DATABASE_NAME = "imagedescbot";
+        private static IMongoDatabase _database;
+
+        public static IMongoDatabase GetDatabase(IConfiguration config)
+        {
+            if (_database != null)
+            {
+                return _database;
+            }
+
+            MongoClient _client = new MongoClient(config.GetConnectionString(CONNECTION_STRING_NAME));
+            _database = _client.GetDatabase(DATABASE_NAME);
+
+            return _database;
+        }
+    }
+}
--- ReportAPI/Startup.cs
+++ ReportAPI/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using ReportAPI.Services;
 
 namespace ReportAPI
 {
@@ -25,6 +26,7 @@ namespace ReportAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddScoped<ImageService>();
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
 
@@ -43,6 +45,7 @@ namespace ReportAPI
 
             app.UseHttpsRedirection();
             app.UseMvc();
+            app.UseResponseBuffering();
         }
     }
 }
